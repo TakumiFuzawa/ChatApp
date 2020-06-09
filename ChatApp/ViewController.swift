aba9cb9723b55f5fec47d7f0ddd39ced50cd56a0
github TakumiFuzawa <futaku0811@gmail.com>
2020-06-10 01:29:46 +0900

    layout

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -49,7 +49,32 @@ class ViewController: UIViewController {
         termsOfServiceLable.attributedText = attributedTermsText
         termsOfServiceLable.numberOfLines = 0
         
+        //buttonのアイコン,レイアウトの設定
+        signInFacebookButton.setTitle("Sign in with Facebook", for: UIControl.State.normal)
+        signInFacebookButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        signInFacebookButton.backgroundColor = UIColor(red: 58 / 255, green: 85 / 255, blue: 159 / 255, alpha: 1)
+        signInFacebookButton.layer.cornerRadius = 5
+        signInFacebookButton.clipsToBounds = true
+        signInFacebookButton.setImage(UIImage(named: "icon-facebook"), for: UIControl.State.normal)
+        signInFacebookButton.imageView?.contentMode = .scaleAspectFit
+        signInFacebookButton.tintColor = .white
+        signInFacebookButton.imageEdgeInsets = UIEdgeInsets(top: 12, left: -15, bottom: 12, right: 0)
         
+        signInGoogleButton.setTitle("Sign in with Google", for: UIControl.State.normal)
+        signInGoogleButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        signInGoogleButton.backgroundColor = UIColor(red: 223 / 255, green: 74 / 255, blue: 50 / 255, alpha: 1)
+        signInGoogleButton.layer.cornerRadius = 5
+        signInGoogleButton.clipsToBounds = true
+        signInGoogleButton.setImage(UIImage(named: "icon-google"), for: UIControl.State.normal)
+        signInGoogleButton.imageView?.contentMode = .scaleAspectFit
+        signInGoogleButton.tintColor = .white
+        signInGoogleButton.imageEdgeInsets = UIEdgeInsets(top: 12, left: -30, bottom: 12, right: 0)
+        
+        createNewAcountButton.setTitle("Create a new acount", for: UIControl.State.normal)
+        createNewAcountButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        createNewAcountButton.backgroundColor = UIColor.black
+        createNewAcountButton.layer.cornerRadius = 5
+        createNewAcountButton.clipsToBounds = true
     }
     
     
