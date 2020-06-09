8217ec7487b1d06ff828f75b8bf81295e1829898
github TakumiFuzawa <futaku0811@gmail.com>
2020-06-10 00:59:33 +0900

    first commit

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var titleLable: UILabel!
+    
+    @IBOutlet var signInFacebookButton: UIButton!
+    
+    @IBOutlet var signInGoogleButton: UIButton!
+    
+    @IBOutlet var orLable: UILabel!
+    
+    @IBOutlet var createNewAcountButton: UIButton!
+    
+    @IBOutlet var termsOfServiceLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        let title = "Create New Acount"
+        let subtitle = "\n\n\nHello World"
+        //fontのレイアウト
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font : UIFont.init(name: "Didot", size: 28)!, NSAttributedString.Key.foregroundColor : UIColor.black])
+        let attributedSubTitle = NSMutableAttributedString(string: subtitle, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.45)])
+        attributedText.append(attributedSubTitle)
+        //行間を5行あける
+        let pargrapStyle = NSMutableParagraphStyle()
+        pargrapStyle.lineSpacing = 5
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: pargrapStyle, range: NSMakeRange(0, attributedSubTitle.length))
+        titleLable.numberOfLines = 0
+        
+        titleLable.attributedText = attributedText
+        
+        let attributedTermsText = NSMutableAttributedString(string: "By clicking 'Create a new acount' you agree to our", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor : UIColor(white: 0, alpha: 0.65)])
+        attributedText.append(attributedTermsText)
+        
+        termsOfServiceLable.attributedText = attributedTermsText
+        termsOfServiceLable.numberOfLines = 0
+        
+        
+    }
+    
+    
 }
 
