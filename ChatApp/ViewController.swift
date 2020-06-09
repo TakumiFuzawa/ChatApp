f77d3fe3bf66a3329dd9995a30fed3562bac6929
github TakumiFuzawa <futaku0811@gmail.com>
2020-06-09 22:55:17 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Takumi Fuzawa on 2020/06/09.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
